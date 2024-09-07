b421553262095abb53eb02e4b700e8bfcf3287c0
shiki <kishintorii@gmail.com>
2024-09-07 16:03:14 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), icon()],
-    site: `https://misc-org.github.io`,
-    base: '/RuleBook-Front',
+    site: `https://misc-org.github.io/RuleBook-Front`,
+    base: '/',
     redirects: {
         '/': '/start/start',
     }

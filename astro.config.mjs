00027962c205c0a155a5221ea699ff1388753f4e
shiki <kishintorii@gmail.com>
2024-09-07 16:23:07 +0900

    layout & config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), icon()],
     site: `https://misc-org.github.io/`,
-    base: '/RuleBook-Front',
+    base: `/RuleBook-Front`,
     redirects: {
         '/': '/RuleBook-Front/start/start',
     }

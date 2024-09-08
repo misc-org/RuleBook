0220bc10f06030bd08e64a4144c89ece7be4330a
shiki <kishintorii@gmail.com>
2024-09-08 19:36:53 +0900

    fix link

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,8 +1,10 @@
 import Blockquote from './component/mdx/Blockquote.astro'
+import A from './component/mdx/A.astro'
 import AstroIcon from './component/mdx/Icon.astro'
 
 export const compo = {
     blockquote: Blockquote,
+    a: A,
 }
 
 export {AstroIcon}
